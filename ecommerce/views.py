d7b8a1645fb31050a4b969bcee1ca86b77b743d0
lazarmiric98 <miriclazar2@gmail.com>
2021-01-03 01:01:45 +0100

    First commit

---
+++ ecommerce/views.py
@@ -0,0 +1,107 @@
+from django.contrib.auth import authenticate, login, get_user_model
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+
+from .forms import ContactForm, LoginForm, RegisterForm
+
+
+def home_page(request):
+    context = {
+        "title": "eCommerce",
+    }
+    if request.user.is_authenticated:
+        context["premium_content"] = "YEAHHHhhh"
+
+    return render(request, "home_page.html", context)
+
+
+def about_page(request):
+    context = {
+        "title": "About page",
+        "content": "Welcome to about page"
+    }
+    return render(request, "home_page.html", context)
+
+
+def contact_page(request):
+    form = ContactForm(request.POST or None)
+    context = {
+        "title": "Contact",
+        "form": form 
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+    return render(request, "contact/view.html", context)
+
+
+def login_page(request):
+    form = LoginForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    print("User logged in")
+    # print(request.user.is_authenticated())
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password")
+        user = authenticate(request, username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            # Redirect to a success page.
+            return redirect("/")
+        else:
+            # Return an 'invalid login' error message.
+            print("Error")
+
+    return render(request, "auth/login.html", context)
+
+User = get_user_model()
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username,email,password)
+        print(new_user)
+    return render(request, "auth/register.html", context)
+
+
+def home_page_old(request):
+    html_ = """
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <!-- Required meta tags -->
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+
+            <!-- Bootstrap CSS -->
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+            <title>Hello, world!</title>
+        </head>
+        <body>
+            <div class='text-center'>
+                <h1>Hello, world!</h1>
+            </dev>
+            <!-- Optional JavaScript; choose one of the two! -->
+
+            <!-- Option 1: Bootstrap Bundle with Popper -->
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+            <!-- Option 2: Separate Popper and Bootstrap JS -->
+            <!--
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js" integrity="sha384-q2kxQ16AaE6UbzuKqyBE9/u/KzioAlnx2maXQHiDX9d4/zp8Ok3f+M7DPm+Ib6IU" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-pQQkAEnwaBkjpqZ8RU1fF1AKtTcHJwFl3pblpTlHXybJjHpMYo79HY3hIi4NKxyj" crossorigin="anonymous"></script>
+            -->
+        </body>
+    </html>
+    """
+    return HttpResponse(html_)
